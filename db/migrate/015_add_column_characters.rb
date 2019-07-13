0ee664899161f8c9fc389fbb114d176c1149a760
Abagail Hudson <abagailanne@gmail.com>
2019-07-13 22:05:25 +0000

    Automatically backed up by Learn

---
+++ db/migrate/015_add_column_characters.rb
@@ -0,0 +1,7 @@
+class AddColumnsCharacters < ActiveRecord::Migration[4.2]
+  def change
+    add_column :characters, :show_id, :integer
+    add_column :characters, :actor_id, :integer
+    end
+  end
+end 
